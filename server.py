bf54674cb53277a46ff2faf9c8022df1db21e7bf
Evan Purcer <evanpurcer@gmail.com>
2015-08-31 19:24:24 -0700

    Do querying in background so page load is faster

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
+import json, threading
 from flask import Flask, render_template, jsonify
-app = Flask(__name__)
 
 from query_client import make_query
+
+app = Flask(__name__)
 
 @app.route('/')
 def main():
@@ -9,9 +11,31 @@ def main():
 
 @app.route('/query')
 def query():
-    result = make_query('static/js/multi_funnel_query.js')
-    print result
-    return jsonify(**result)
+    return jsonify(**query_and_format_data())
+
+@app.route('/data')
+def data():
+    data = {}
+
+    try:
+        data_file = open('data.json')
+    except: # no data file, we'll just have to wait for the query to finish
+        data = query_and_format_data()
+    else:
+        with data_file:
+            data = json.loads(data_file.read())
+        # start a query in the background to update data file for next load
+        threading.Thread(target=query_and_format_data).start()
+
+    return jsonify(**data)
+
+def query_and_format_data():
+    data = make_query('static/js/multi_funnel_query.js')
+
+    with open('data.json', 'w') as f:
+        f.write(json.dumps(data))
+
+    return data
 
 if __name__ == '__main__':
     app.run(debug=True)
